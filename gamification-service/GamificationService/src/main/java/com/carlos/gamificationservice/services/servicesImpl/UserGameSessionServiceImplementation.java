2bd76823c9fa351981b200264e50db1392a3d96d
Carlos Ivan <93873199+camunozv@users.noreply.github.com>
2026-03-13 21:45:14 -0500

    feat: finished UserGameSessionService implementation, pending to connect it and test it

--- gamification-service/GamificationService/src/main/java/com/carlos/gamificationservice/services/servicesImpl/UserGameSessionServiceImplementation.java
+++ gamification-service/GamificationService/src/main/java/com/carlos/gamificationservice/services/servicesImpl/UserGameSessionServiceImplementation.java
@@ -1,11 +1,13 @@
 package com.carlos.gamificationservice.services.servicesImpl;
 
+import com.carlos.gamificationservice.dtos.UserGameSessionMapper;
 import com.carlos.gamificationservice.dtos.dtosImpl.UserGameSessionDTO;
 import com.carlos.gamificationservice.models.UserGameSession;
 import com.carlos.gamificationservice.repository.UserGameSessionRepository;
 import com.carlos.gamificationservice.services.UserGameSessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,34 +17,69 @@ import java.util.List;
 public class UserGameSessionServiceImplementation implements UserGameSessionService {
 
     private final UserGameSessionRepository userGameSessionRepository;
+    private final UserGameSessionMapper userGameSessionMapper;
 
     @Override
     public boolean saveUserGameSession(UserGameSessionDTO newUserGameSession) {
-        return false;
+
+        boolean result;
+
+        try {
+            UserGameSession userGameSession = userGameSessionMapper.toUserGameSession(newUserGameSession);
+            userGameSessionRepository.save(userGameSession); // Saves new game session data into the database.
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+
+        return result;
     }
 
     @Override
+    @Transactional
     public boolean deleteAllUserGameSessions(String userName) {
-        return false;
+        boolean result;
+
+        try {
+            userGameSessionRepository.deleteUserGameSessionsByUserName(userName);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+
+        return result;
     }
 
     @Override
+    @Transactional
     public boolean deleteAllUserGameSessionsPerDate(String userName, LocalDate intendedDate) {
-        return false;
+        boolean result;
+
+        try {
+            userGameSessionRepository.deleteByUserNameAndSessionDate(userName, intendedDate);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = false;
+        }
+
+        return result;
     }
 
     @Override
     public List<UserGameSession> getAllUserGameSessions(String userName) {
-        return List.of();
+        return userGameSessionRepository.getAllByUserName(userName);
     }
 
     @Override
     public List<UserGameSession> getAllUserGameSessionsPerDate(String userName, LocalDate intendedDate) {
-        return List.of();
+        return userGameSessionRepository.getAllByUserNameAndSessionDate(userName, intendedDate);
     }
 
     @Override
     public List<UserGameSession> getAllUserGameSessionsPerPoints(String userName, Integer intendedPoints) {
-        return List.of();
+        return userGameSessionRepository.getAllByUserNameAndPoints(userName, intendedPoints);
     }
 }
